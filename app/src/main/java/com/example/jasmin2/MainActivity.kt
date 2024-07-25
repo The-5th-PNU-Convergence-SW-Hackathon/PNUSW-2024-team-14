7953b4f1af0b7685774a606b670193b2392633eb
jinseomun <jshin2702@naver.com>
2024-07-25 19:09:54 +0900

    수정2

--- app/src/main/java/com/example/jasmin2/MainActivity.kt
+++ app/src/main/java/com/example/jasmin2/MainActivity.kt
@@ -7,13 +7,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jasmin2.ui.theme.MyDetailpage
 import com.example.jasmin2.ui.theme.MyScroll
+import com.example.jasmin2.ui.theme.StartScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyScroll()
-            MyDetailpage()
+          StartScreen()
         }
     }
 }
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyAppContentPreview() {
     //MyScroll()
-    MyDetailpage()
+  StartScreen()
 }
 
 
